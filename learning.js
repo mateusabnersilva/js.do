2a6e65c6a68dd72461a2a6cb5c646acc31b3c493
Mateus Abner Silva <mateusabner1025@gmail.com>
2026-08-14 09:55:50 -0300

    Initial commit
    A new view in js.

---
+++ learning.js
@@ -0,0 +1,23 @@
+//Js.do learning
+
+// Duas barras para comentários ou /*
+
+<script>
+window.alert('')
+window.confirm('')
+window.prompt('')
+</script>
+
+// Nomeando variaveis 
+var n1 = 1
+var n2 = 2
+var n3 = 3
+
+var s1 = "Javascript"
+var s2 = "Curso em Video"
+var s3 = "Guanabara"
+
+
+
+var nome = window.prompt('Qual é o seu nome?')
+window.alert("É um prazer em te conhecer, " + nome)
